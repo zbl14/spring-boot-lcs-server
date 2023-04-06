a52cd239fea96233540149d1e483ceae9e95f0f4
Zhibin Liang <ifthereisoneday@gmail.com>
2023-04-05 22:16:53 -0400

    initial the project

---
+++ src/main/java/com/lcs/server/SpringBootLcsServerApplication.java
@@ -0,0 +1,13 @@
+package com.lcs.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLcsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLcsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcs/server/SpringBootLcsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcs.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLcsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
